0f9568eb1bfb74e46718cb41b96561c2ae4336ee
Sarthak <sarthakmjh@gmail.com>
2024-01-26 14:57:28 +0530

    foreach loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,43 @@
+// foreach loop 
+
+const coding=["js","ruby","java","python","cpp"]
+
+// coding.forEach(function (val){
+//     console.log(val)
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item)
+// })
+
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+// in this we are only giving reference, we are not executing the function
+
+coding.forEach((item,index,arr)=>{
+    // console.log(item,index,arr)
+
+})
+
+const myCoding=[
+    {
+        languageName: "javaScript",
+        languageFileName:"js"
+    },
+    {
+        languageName: "java",
+        languageFileName:"java"
+    },
+    {
+        languageName: "python",
+        languageFileName:"py"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName)
+})

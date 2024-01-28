fd7653387f73d15480bd5953de81acf7c4d0555c
Sarthak <sarthakmjh@gmail.com>
2024-01-28 20:57:35 +0530

    myCLassses in js

---
+++ 10_classes_oops/myClasses.js
@@ -0,0 +1,42 @@
+// after ES6
+
+// class User{
+//     constructor(username,email,password){
+//         this.username=username
+//         this.email=email
+//         this.password=password
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+
+//     changeUserName(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai=new User("Chai","chai@gmail.com","123")
+// console.log(chai.encryptPassword())
+// console.log(chai.changeUserName())
+
+
+// behind the scene
+
+function User(username,email,password){
+    this.username=username
+    this.email=email
+    this.password=password
+}
+
+User.prototype.encryptPassword=function(){
+    return `${this.password}abc`
+}
+
+User.prototype.changeUserName=function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const Tea=new User("Tea","Tea@gmail.com","123")
+console.log(Tea.encryptPassword())
+console.log(Tea.changeUserName())
